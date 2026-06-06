34fea26d447919bb944e9fe1ed91aeaff0863197
wikspeck <wik.speck@hotmail.com>
2026-06-06 14:20:28 +0200

    Replace header SVG with PNG logo
    Add new PNG logo asset (public/yimagelogoheader.png) and update Header.jsx to use /yimagelogoheader.png instead of /Yimarealgelogodark.svg. This swaps the header image source to the new PNG file with no other functional changes.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -100,7 +100,7 @@ export default function Header({
           <button type="button" className="brand-lockup" onClick={onHome} aria-label="Go to Yimage home">
             <Box
               component="img"
-              src="/Yimarealgelogodark.svg"
+              src="/yimagelogoheader.png"
               alt="Yimage"
               sx={{
                 width: { xs: 50, sm: 56 },
